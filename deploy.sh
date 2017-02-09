df63b9bf60840164f2ac671187f71c64d5280800
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2017-02-09 14:30:04 +0000

    Fix for more DSP tinkering

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ export KUBE_SERVER=${KUBE_SERVER_DEV}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
-kd --insecure-skip-tls-verify --retries=20 \
+kd --insecure-skip-tls-verify --timeout 5m0s \
    --file ${ENVIRONMENT}/pttg-fs-stub-deployment.yaml \
    --file ${ENVIRONMENT}/pttg-fs-stub-svc.yaml
