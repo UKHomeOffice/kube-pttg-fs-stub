7f9fac06f44c14946bdf53595d9b86245ac61eac
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2017-02-14 13:16:59 +0000

    Add prod deploy to UI and tidy and make deploys consistent

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,15 +5,6 @@ export APP=pttg-fs-stub
 export KUBE_SERVER=${KUBE_SERVER_PROD}
 export KUBE_TOKEN=${KUBE_TOKEN_PROD}
 
-echo ${KUBE_SERVER_DEV}
-
-if [ -z ${KUBE_TOKEN_PROD+x} ]
-then
-    echo "KUBE_TOKEN_PROD must be set"
-else
-    echo "KUBE_TOKEN_PROD is set"
-fi
-
 cd kd
 kd --debug \
    --insecure-skip-tls-verify --timeout 5m0s \
