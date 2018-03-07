c6a9d8076c3600fecda5e64747f0166fd8a5e490
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2018-03-07 22:37:31 +0000

    ACP_MIGRATION: correct secrets

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,24 @@
 
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
-export KUBE_TOKEN=${KUBE_TOKEN}
+
+if [[ ${ENVIRONMENT} == "test" ]] ; then
+    echo "deploy ${VERSION} to test namespace, using PTTG_FS_TEST drone secret"
+    export KUBE_TOKEN=${PTTG_FS_TEST}
+else
+    echo "deploy ${VERSION} to dev namespace, using PTTG_FS_DEV drone secret"
+    export KUBE_TOKEN=${PTTG_FS_DEV}
+fi
+
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
 
 cd kd
+
 kd --insecure-skip-tls-verify \
    -f networkPolicy.yaml \
    -f deployment.yaml \
